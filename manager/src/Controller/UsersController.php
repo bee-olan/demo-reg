4cbfd01bf43abcbe54dc9ea570dd31d818a4c870
olan <bee.olan@mail.ru>
2024-06-30 22:05:19 +0300

    Это демо версия

--- manager/src/Controller/UsersController.php
+++ manager/src/Controller/UsersController.php
@@ -13,7 +13,7 @@ use App\Model\User\UseCase\Role;
 use App\Model\User\UseCase\SignUp\Confirm;
 use App\ReadModel\User\Filter;
 use App\ReadModel\User\UserFetcher;
-use App\ReadModel\Work\Members\Member\MemberFetcher;
+//use App\ReadModel\Work\Members\Member\MemberFetcher;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -243,13 +243,13 @@ class UsersController extends AbstractController
     /**
      * @Route("/{id}", name=".show")
      * @param User $user
-     * @param MemberFetcher $members
+//     * @param MemberFetcher $members
      * @return Response
      */
-    public function show(User $user, MemberFetcher $members): Response
+    public function show(User $user): Response
     {
-        $member = $members->find($user->getId()->getValue());
+//        $member = $members->find($user->getId()->getValue());
 
-        return $this->render('app/users/show.html.twig', compact('user', 'member'));
+        return $this->render('app/users/show.html.twig', compact('user'));
     }
 }
